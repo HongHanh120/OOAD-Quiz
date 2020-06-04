28cca2a4211e3346d0c8859a46f612a78a314a12
Hanh <lehonghanh.120@gmail.com>
2020-06-04 18:03:50 +0700

    hna

--- src/components/Question.js
+++ src/components/Question.js
@@ -8,15 +8,17 @@ class Question extends Component {
 
     render() {
         const {question, index} = this.props;
-        const answers = question["answers"].sort(() => 0.5 - Math.random());
+        const answers = question["answers"];
+        // const answers = question.answers;
+        // const answers = question["answers"].sort(() => 0.5 - Math.random());
         return (
             <div className= "question">
                 <div className="form-group pt-2">
                     <label className="font-weight-seminole"><strong>{index + 1}. {question["cont"]}</strong></label>
                     {
-                        answers.map(answer =>{
+                        answers.map((answer, index) =>{
                             return <Answer key={answer.key} answer={answer}
-                                            corrects={question.corrects}/>
+                                            corrects={question.corrects}  index={index}/>
                         })
                     }
                 </div>
