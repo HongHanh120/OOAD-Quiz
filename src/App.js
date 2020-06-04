22828c1a3224f796fc6f97f7c42dd98ea1050821
Hanh <lehonghanh.120@gmail.com>
2020-06-04 12:52:09 +0700

    anh

--- src/App.js
+++ src/App.js
@@ -3,34 +3,19 @@ import './App.css';
 
 import {HashRouter as Router, Route, Link} from 'react-router-dom';
 
-import {Container, Pagination, Jumbotron} from "react-bootstrap";
+import {Container, Jumbotron} from "react-bootstrap";
 
 import QuestionList from "./components/QuestionList";
 
-import questions1 from "./questions/ooad/ooad1";
-import questions2 from "./questions/ooad/ooad2";
-// import questions3 from "./questions/ooad/ooad3";
+import questions from "./questions/ooad/ooad";
 
 function App() {
   return (
     <Router>
       <Container>
         <Jumbotron>
-          <Pagination>
-            <Pagination.Item><Link to="/">1</Link></Pagination.Item>
-            <Pagination.Item><Link to="/2">2</Link></Pagination.Item>
-            {/*<Pagination.Item><Link to="/3">3</Link></Pagination.Item>*/}
-          </Pagination>
+          <Route path="/" exact component={() => <QuestionList questions={questions} />} />
 
-          <Route path="/" exact component={() => <QuestionList questions={questions1} />} />
-          <Route path="/2" component={() => <QuestionList questions={questions2} />} />
-          {/*<Route path="/3" component={() => <QuestionList questions={questions3} />} />*/}
-
-          <Pagination>
-            <Pagination.Item><Link to="/">1</Link></Pagination.Item>
-            <Pagination.Item><Link to="/2">2</Link></Pagination.Item>
-            {/*<Pagination.Item><Link to="/3">3</Link></Pagination.Item>*/}
-          </Pagination>
         </Jumbotron>
       </Container>
     </Router>
--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -6,13 +6,27 @@ import Question from "./Question";
 
 import PubSub from "pubsub-js";
 
-import ooad from "../questions/ooad/ooad1";
-// import ooad2 from "../questions/ooad/ooad2";
+import ooad from "../questions/ooad/ooad";
+
+import { Pagination, PaginationItem, PaginationLink } from 'reactstrap';
 
 class QuestionList extends Component{
+    constructor() {
+        super();
 
+        this.dataSet = this.makeAllQuestions();
+        this.totalPages = this.dataSet.length;
+        this.pageSize = 20;
+        this.pagesCount = Math.ceil(this.totalPages / this.pageSize);
+
+        this.state = {currentPage: 0};
+    }
+
+    handleClick(e, index) {
+        e.preventDefault();
+        this.setState({currentPage: index});
+    }
     checkAnswer = () => {
-        // this.setState({checked:true});
         PubSub.publish("SUMMIT_ANSWER", 'submit_answer')
     };
 
@@ -25,22 +39,67 @@ class QuestionList extends Component{
     };
 
     render() {
-        const questions = this.makeAllQuestions();
+        // const questions = this.makeAllQuestions();
+        const { currentPage } = this.state;
         return (
             <div className="questions">
-                {
-                    questions.map((question, i) => {
-                        return (
-                            <Question
-                                key={i}
-                                question={question}
-                                index={i}
+                <div className="pagination-wrapper">
+                    <Pagination aria-label="Page navigation example">
+                        <PaginationItem disabled={currentPage <= 0}>
+                            <PaginationLink
+                                onClick={e => this.handleClick(e, currentPage - 1)}
+                                previous
+                                href="#"
                             />
-                        )
-                    })
+                        </PaginationItem>
+
+                        {[...Array(this.pagesCount)].map((page, i) =>
+                            <PaginationItem active={i === currentPage} key={i}>
+                                <PaginationLink onClick={e => this.handleClick(e,i)} href="#">
+                                    {i + 1}
+                                </PaginationLink>
+                            </PaginationItem>
+                        )}
+
+                        <PaginationItem disabled={currentPage >= this.pagesCount - 1}>
+                            <PaginationLink
+                                onClick={e => this.handleClick(e, currentPage + 1)}
+                                next
+                                href="#"
+                            />
+                        </PaginationItem>
+
+                    </Pagination>
+                </div>
+                {
+                    this.dataSet.slice(currentPage * this.pageSize, (currentPage + 1) * this.pageSize)
+                        .map((question, i) => {
+                            return (
+                                <Question
+                                    key={i}
+                                    question={question}
+                                    index={i}
+                                />
+                            )
+                        })
                 }
                 <Button variant="primary" onClick={this.checkAnswer}>Submit</Button>
                 <hr />
+                {/*<div className="questions">*/}
+                {/*    {*/}
+                {/*        questions.map((question, i) => {*/}
+                {/*            return (*/}
+                {/*                <Question*/}
+                {/*                    key={i}*/}
+                {/*                    question={question}*/}
+                {/*                    index={i}*/}
+                {/*                />*/}
+                {/*            )*/}
+                {/*        })*/}
+                {/*    }*/}
+                {/*    <Button variant="primary" onClick={this.checkAnswer}>Submit</Button>*/}
+                {/*    <hr />*/}
+                {/*</div>*/}
             </div>
         );
     }
